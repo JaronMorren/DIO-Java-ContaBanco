51bdc4f8724c0154cdb5d9f1d4264af5dff35dfb
Jaron Morren <jaronmorren@gmail.com>
2024-06-05 10:13:23 -0300

    created a bank account class that prints user input

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int Numero;
+        String Agencia;
+        String NomeCliente;
+        Double Saldo;
+        System.out.print("Por favor, digite o número da conta: ");
+        Numero = scanner.nextInt();
+        System.out.print("Por favor, digite o número da agência: ");
+        Agencia = scanner.next();
+        System.out.print("Por favor, digite o seu nome: ");
+        NomeCliente = scanner.next();
+        System.out.print("Por favor, digite o saldo da conta: ");
+        Saldo = scanner.nextDouble();
+        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + Agencia + ", conta " + Numero + " e seu saldo " + Saldo + " já está disponível para saque.");
+        scanner.close();
+
     }
 }
